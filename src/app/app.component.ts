7766067414f16a6ae6f763f0d2853cb97d4ab3e0
Pagalavan7 <pagalavanvetri@gmail.com>
2025-01-30 09:06:41 +0530

    if and for conditional formatting complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,19 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { userDetails } from './user/userDetails';
 import { CommonModule } from '@angular/common';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, CommonModule],
+  imports: [HeaderComponent, UserComponent, CommonModule, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   userDetails = userDetails;
+  name: string = '';
 
   onSelectUser(name: string) {
-    console.log(`User ${name} is selected`);
+    this.name = name;
   }
 }
